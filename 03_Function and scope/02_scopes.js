c795cfa9d07879e01ba38021bde759232dc4c618
Amandeep Singh <amandeepronaldo@gmail.com>
2025-07-30 09:19:44 +0530

    Scopes.js

---
+++ 03_Function and scope/02_scopes.js
@@ -0,0 +1,49 @@
+//var c = 300
+let a = 300;
+if (true) {
+  let a = 10;
+  const b = 20;
+  // console.log("INNER: ", a);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one() {
+  const username = "hitesh";
+
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  // console.log(website);
+
+  two();
+}
+
+// one()
+
+if (true) {
+  const username = "hitesh";
+  if (username === "hitesh") {
+    const website = " youtube";
+    console.log(username + website);
+  }
+//   console.log(website);             // error 
+}
+
+console.log(username);              // error
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+console.log(addone(5));  // here we can access before declaration
+
+function addone(num) {
+  return num + 1;
+}
+
+addTwo(5);                      // but here it will throw error
+const addTwo = function (num) {
+  return num + 2;
+};
