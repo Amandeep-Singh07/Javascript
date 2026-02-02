bbd77f1818a91a4badb4fe6e4ed0d273d037f0fd
Amandeep Singh <amandeepronaldo@gmail.com>
2026-02-02 12:57:07 +0530

    Digital Clock

--- 07_03_project/script.js
+++ 07_03_project/script.js
@@ -1,29 +1,8 @@
-//generate a random color
+const clock = document.getElementById("clock");
+// const clock = document.querySelector('#clock')
 
-const randomColor = function () {
-  const hex = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += hex[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
-let intervalId;
-const startChangingColor = function () {
-  if (!intervalId) {
-    intervalId = setInterval(changeBgColor, 1000);
-  }
-
-  function changeBgColor() {
-    document.body.style.backgroundColor = randomColor();
-  }
-};
-const stopChangingColor = function () {
-  clearInterval(intervalId);
-  intervalId = null;
-};
-
-document.querySelector("#start").addEventListener("click", startChangingColor);
-
-document.querySelector("#stop").addEventListener("click", stopChangingColor);
+setInterval(function () {
+  let date = new Date();
+  // console.log(date.toLocaleTimeString());
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
