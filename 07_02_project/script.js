ce9a2deefb1c8f444ba1b7d74f3085eac9ae26b8
Amandeep Singh <amandeepronaldo@gmail.com>
2026-02-02 12:04:18 +0530

    BMI Calculator

--- 07_02_project/script.js
+++ 07_02_project/script.js
@@ -5,8 +5,8 @@ const form = document.querySelector("form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();                            // prevent the default behaviour of form submission
 
-  const height = parseInt(document.querySelector("#height").value);
-  const weight = parseInt(document.querySelector("#weight").value);
+  const height = parseInt(document.querySelector("#height").value); // we have converted the string value to integer
+  const weight = parseInt(document.querySelector("#weight").value);// we have converted the string value to integer
   const results = document.querySelector("#results");
 
   if (height === "" || height < 0 || isNaN(height)) {
@@ -16,6 +16,18 @@ form.addEventListener("submit", function (e) {
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
+    
+    // i want to print both the bmi and overweght or underweight
+    results.innerHTML = `<span>${bmi}  </span>`;
+    if (bmi < 18.5) {
+      results.innerHTML += `<span>You are underweight</span>`;
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      results.innerHTML += `<span>Your weight is normal</span>`;
+    } else if (bmi >= 25 && bmi < 29.9) {
+      results.innerHTML += `<span>You are overweight</span>`;
+    } else {
+      results.innerHTML += `<span>You are obese</span>`;
+    }
+
   }
 });
